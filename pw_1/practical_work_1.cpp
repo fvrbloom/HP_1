e01c7e1fe686691cb46c44cee2bccb287c257380
fvrbloom <dilnaz1327@gmail.com>
2025-12-22 17:01:51 +0500

    lab1 update

--- pw_1/practical_work_1.cpp
+++ pw_1/practical_work_1.cpp
@@ -1,123 +1,262 @@
-
-// practical_work_1.cpp
-// Practical Work 1: Arrays, pointers and OpenMP
 #include <iostream>
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
+#include <climits>
 #include <omp.h>
+
 using namespace std;
 
-// Sequential average calculation
-double calculateAverageSequential(int* array, int size) {
-    long long sum = 0;
-    for (int i = 0; i < size; ++i) {
-        sum += array[i];
+// ---------- Linked List ----------
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+public:
+    LinkedList() : head(nullptr) {}
+
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* cur = head;
+        while (cur->next) {
+            cur = cur->next;
+        }
+        cur->next = newNode;
     }
-    return static_cast<double>(sum) / size;
+
+    bool remove(int value) {
+        if (!head) return false;
+
+        if (head->data == value) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return true;
+        }
+
+        Node* cur = head;
+        while (cur->next && cur->next->data != value) {
+            cur = cur->next;
+        }
+
+        if (cur->next) {
+            Node* temp = cur->next;
+            cur->next = cur->next->next;
+            delete temp;
+            return true;
+        }
+        return false;
+    }
+
+    bool search(int value) {
+        Node* cur = head;
+        while (cur) {
+            if (cur->data == value) return true;
+            cur = cur->next;
+        }
+        return false;
+    }
+
+    void display() {
+        Node* cur = head;
+        while (cur) {
+            cout << cur->data << " -> ";
+            cur = cur->next;
+        }
+        cout << "NULL\n";
+    }
+
+    ~LinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+// ---------- Stack ----------
+class Stack {
+private:
+    Node* top;
+public:
+    Stack() : top(nullptr) {}
+
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (isEmpty()) return -1;
+        Node* temp = top;
+        int val = temp->data;
+        top = top->next;
+        delete temp;
+        return val;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+
+    void display() {
+        Node* cur = top;
+        while (cur) {
+            cout << cur->data << " ";
+            cur = cur->next;
+        }
+        cout << "\n";
+    }
+
+    ~Stack() {
+        while (!isEmpty()) pop();
+    }
+};
+
+// ---------- Queue ----------
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+public:
+    Queue() : front(nullptr), rear(nullptr) {}
+
+    void enqueue(int value) {
+        Node* newNode = new Node(value);
+        if (!rear) {
+            front = rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    int dequeue() {
+        if (isEmpty()) return -1;
+        Node* temp = front;
+        int val = temp->data;
+        front = front->next;
+        if (!front) rear = nullptr;
+        delete temp;
+        return val;
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+
+    void display() {
+        Node* cur = front;
+        while (cur) {
+            cout << cur->data << " ";
+            cur = cur->next;
+        }
+        cout << "\n";
+    }
+
+    ~Queue() {
+        while (!isEmpty()) dequeue();
+    }
+};
+
+
+
+double averageSequential(int* arr, int n) {
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return (double)sum / n;
 }
 
-// Parallel average calculation
-double calculateAverageParallel(int* array, int size) {
+double averageParallel(int* arr, int n) {
     long long sum = 0;
     #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < size; ++i) {
-        sum += array[i];
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
     }
-    return static_cast<double>(sum) / size;
+    return (double)sum / n;
 }
 
-int main() {
-    int N;
-    cout << "Enter array size N: ";
-    cin >> N;
-    
-    if (N <= 0) {
-        cout << "Invalid array size\n";
-        return 1;
-    }
 
-    // Dynamic memory allocation
+int main() {
+    srand((unsigned)time(nullptr));
+
+    int N;
+    cout << "Enter array size: ";
+    cin >> N;
+
+    if (N <= 0) return 1;
+
     int* data = new int[N];
-    srand(static_cast<unsigned>(time(nullptr)));
-    
-    // Fill array with values [1..100]
-    for (int i = 0; i < N; ++i) {
+
+    for (int i = 0; i < N; i++) {
         data[i] = rand() % 100 + 1;
     }
 
-    // Output array (only for small arrays)
-    if (N <= 100) {
-        cout << "Array elements:\n";
-        for (int i = 0; i < N; ++i) {
-            cout << data[i] << " ";
-        }
-        cout << "\n\n";
-    } else {
-        cout << "Array created with " << N << " elements.\n\n";
-    }
-
-    // -------------------------------
-    // Part 1: min/max
-    // -------------------------------
-    
-    // Sequential
+    // ---------- MIN / MAX ----------
     auto t1 = chrono::high_resolution_clock::now();
-    int minSeq = data[0];
-    int maxSeq = data[0];
-    for (int i = 1; i < N; ++i) {
+    int minSeq = data[0], maxSeq = data[0];
+    for (int i = 1; i < N; i++) {
         if (data[i] < minSeq) minSeq = data[i];
         if (data[i] > maxSeq) maxSeq = data[i];
     }
     auto t2 = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> timeSeq = t2 - t1;
 
-    // Parallel
-    int minPar = data[0];
-    int maxPar = data[0];
+    int minPar = INT_MAX, maxPar = INT_MIN;
     auto t3 = chrono::high_resolution_clock::now();
     #pragma omp parallel for reduction(min:minPar) reduction(max:maxPar)
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++) {
         if (data[i] < minPar) minPar = data[i];
         if (data[i] > maxPar) maxPar = data[i];
     }
     auto t4 = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> timePar = t4 - t3;
 
-    cout << "=== Min/Max Search ===" << endl;
-    cout << "Sequential: min = " << minSeq
-         << ", max = " << maxSeq
-         << ", time = " << timeSeq.count() << " ms\n";
-    cout << "Parallel:   min = " << minPar
-         << ", max = " << maxPar
-         << ", time = " << timePar.count() << " ms\n";
-    cout << "Speedup: " << timeSeq.count() / timePar.count() << "x\n\n";
+    cout << "Sequential min/max: " << minSeq << " / " << maxSeq << endl;
+    cout << "Parallel   min/max: " << minPar << " / " << maxPar << endl;
 
-    // -------------------------------
-    // Part 3: average value
-    // -------------------------------
-    
-    // Sequential average
+    // ---------- AVERAGE ----------
     auto t5 = chrono::high_resolution_clock::now();
-    double avgSeq = calculateAverageSequential(data, N);
+    double avgSeq = averageSequential(data, N);
     auto t6 = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> timeAvgSeq = t6 - t5;
 
-    // Parallel average
     auto t7 = chrono::high_resolution_clock::now();
-    double avgPar = calculateAverageParallel(data, N);
+    double avgPar = averageParallel(data, N);
     auto t8 = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> timeAvgPar = t8 - t7;
 
-    cout << "=== Average Calculation ===" << endl;
-    cout << "Sequential: avg = " << avgSeq 
-         << ", time = " << timeAvgSeq.count() << " ms\n";
-    cout << "Parallel:   avg = " << avgPar 
-         << ", time = " << timeAvgPar.count() << " ms\n";
-    cout << "Speedup: " << timeAvgSeq.count() / timeAvgPar.count() << "x\n";
+    cout << "Sequential average: " << avgSeq << endl;
+    cout << "Parallel   average: " << avgPar << endl;
 
-    // Free memory
+    // ---------- DATA STRUCTURES ----------
+    LinkedList list;
+    list.append(10);
+    list.append(20);
+    list.append(30);
+    list.display();
+
+    Stack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.display();
+
+    Queue queue;
+    queue.enqueue(5);
+    queue.enqueue(6);
+    queue.enqueue(7);
+    queue.display();
+
     delete[] data;
-    
     return 0;
 }
